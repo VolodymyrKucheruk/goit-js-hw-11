9dcb2e3e4ebd9b2abe2241585c9d45132e16eb16
VolodymyrKucheruk <voloda.2014a@gmail.com>
2023-12-19 01:58:13 +0100

    modefied2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { handleScroll, generatingImages } from './pixabay_api.js';
+import {generatingImages} from './pixabay_api.js';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
@@ -6,63 +6,61 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const formElem = document.querySelector('.search-form');
 const galleryElem = document.querySelector('.gallery');
 const inputElem = document.querySelector('.header_input');
+const loadMoreButton = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.photo-card a');
 
 let page = 1;
 let lastQuery = '';
 let totalHits = 0;
+let isLoading = false;
+const imagesPerPage = 40;
 
 inputElem.addEventListener('input', onInput);
 formElem.addEventListener('submit', onSubmit);
 inputElem.addEventListener('keydown', onKeydown);
-
-handleScroll(loadMore);
+loadMoreButton.addEventListener('click', loadMore);
 
 const scrollToTopButton = createScrollToTopButton();
 
 window.addEventListener('scroll', handleScrollVisibility);
 
-async function onInput() {}
+async function onInput() {};
+
 async function onSubmit(event) {
   event.preventDefault();
-  const inputValue = inputElem.value.trim().toLowerCase();
-  page = 1;
-  galleryElem.innerHTML = '';
-
-  try {
-    const data = await generatingImages(inputValue, page);
-    handleImageData(data);
-  } catch (error) {
-    Notiflix.Notify.failure('An error occurred. Please try again.');
-  }
+  handleSearch();
 }
 
 async function onKeydown(event) {
   if (event.key === 'Enter') {
     event.preventDefault();
-    page = 1;
-    galleryElem.innerHTML = '';
-    const inputValue = inputElem.value.trim().toLowerCase();
-
-    try {
-      const data = await generatingImages(inputValue, page);
-      handleImageData(data);
-    } catch (error) {
-      Notiflix.Notify.failure('An error occurred. Please try again.');
-    }
+    handleSearch();
   }
 }
 
 async function loadMore() {
-  page += 1;
-
-  try {
-    const data = await generatingImages(lastQuery, page);
-    handleImageData(data);
-  } catch (error) {
-    Notiflix.Notify.failure('An error occurred. Please try again.');
+    if (isLoading) {
+      return;
+    }
+  
+    isLoading = true;
+  
+    try {
+      const data = await generatingImages(lastQuery, page, imagesPerPage);
+      handleImageData(data);
+      page += 1;
+  
+      if (page * imagesPerPage >= totalHits) {
+        hideLoadMoreButton();
+        Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+      }
+    } catch (error) {
+      Notiflix.Notify.failure('An error occurred. Please try again.');
+    } finally {
+      isLoading = false;
+    }
   }
-}
+  
 
 function handleImageData(data) {
   if (data.hits.length > 0) {
@@ -70,52 +68,66 @@ function handleImageData(data) {
     const markup = generateImageCardsMarkup(data.hits);
     galleryElem.insertAdjacentHTML('beforeend', markup);
     lightbox.refresh();
-    showTotalHitsMessage();
+    showSuccessMessage();
+    showLoadMoreButton();
   } else {
+    hideLoadMoreButton();
     Notiflix.Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
   }
 
-  inputElem.value = '';
   lastQuery = inputElem.value;
+}
+
+async function handleSearch() {
+  page = 1;
+  galleryElem.innerHTML = '';
+  const inputValue = inputElem.value.trim().toLowerCase();
+
+  try {
+    const data = await generatingImages(inputValue, page, imagesPerPage);
+    handleImageData(data);
+  } catch (error) {
+    Notiflix.Notify.failure('An error occurred. Please try again.');
+  }
 }
 
 function generateImageCardsMarkup(images) {
   return images
     .map(
       img => `
-    <div class="photo-card">
-      <a href="${img.largeImageURL}" class="photo-card-img">
-        <img class="current-img" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
-      </a>
-      <div class="info">
-        <p class="info-item">
-          <b>Likes</b> ${img.likes}
-        </p>
-        <p class="info-item">
-          <b>Views</b> ${img.views}
-        </p>
-        <p class="info-item">
-          <b>Comments</b> ${img.comments}
-        </p>
-        <p class="info-item">
-          <b>Downloads</b> ${img.downloads}
-        </p>
-      </div>
-    </div>
-  `
+        <div class="photo-card">
+          <a href="${img.largeImageURL}" class="photo-card-img">
+            <img class="current-img" src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+          </a>
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b> ${img.likes}
+            </p>
+            <p class="info-item">
+              <b>Views</b> ${img.views}
+            </p>
+            <p class="info-item">
+              <b>Comments</b> ${img.comments}
+            </p>
+            <p class="info-item">
+              <b>Downloads</b> ${img.downloads}
+            </p>
+          </div>
+        </div>
+      `
     )
     .join('');
 }
 
 function createScrollToTopButton() {
-  const scrollToTopButton = document.createElement('button');
-  scrollToTopButton.textContent = 'Up';
-  scrollToTopButton.classList.add('scroll-to-top-button');
-  document.body.appendChild(scrollToTopButton);
-  scrollToTopButton.addEventListener('click', scrollToTop);
-  return scrollToTopButton;
+    const scrollToTopButton = document.createElement('button');
+      scrollToTopButton.textContent = 'Up';
+      scrollToTopButton.classList.add('scroll-to-top-button');
+      document.body.appendChild(scrollToTopButton);
+      scrollToTopButton.addEventListener('click', scrollToTop);
+      return scrollToTopButton;
 }
 
 function scrollToTop() {
@@ -136,6 +148,18 @@ function handleScrollVisibility() {
   }
 }
 
-function showTotalHitsMessage() {
+function showSuccessMessage() {
   Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+}
+
+function showLoadMoreButton() {
+  if (loadMoreButton) {
+    loadMoreButton.classList.add('visible');
+  }
+}
+
+function hideLoadMoreButton() {
+  if (loadMoreButton) {
+    loadMoreButton.classList.remove('visible');
+  }
 }
--- src/pixabay_api.js
+++ src/pixabay_api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export async function generatingImages(query, page) {
+export async function generatingImages(query, page, perPage) {
   try {
     const response = await axios.get('https://pixabay.com/api/', {
       params: {
@@ -8,7 +8,7 @@ export async function generatingImages(query, page) {
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
-        per_page: 40,
+        per_page: perPage,
         q: query,
         page: page,
       },
@@ -19,37 +19,3 @@ export async function generatingImages(query, page) {
   }
 }
 
-export function handleScroll(callback) {
-  let lastScrollTop = 0;
-  const header = document.querySelector('.header');
-  let scrollDirection = '';
-
-  window.addEventListener('scroll', async () => {
-    const { scrollHeight, scrollTop, clientHeight } = document.documentElement;
-
-    if (scrollTop + clientHeight >= scrollHeight - 200) {
-      try {
-        await callback();
-      } catch (error) {
-        console.error('An error occurred while loading more:', error);
-      }
-    }
-
-    const currentScroll =
-      window.pageYOffset || document.documentElement.scrollTop;
-
-    if (currentScroll > lastScrollTop) {
-      if (scrollDirection !== 'down') {
-        scrollDirection = 'down';
-        header.classList.add('hidden');
-      }
-    } else {
-      if (scrollDirection !== 'up') {
-        scrollDirection = 'up';
-        header.classList.remove('hidden');
-      }
-    }
-
-    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
-  });
-}
